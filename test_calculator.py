364fa8e84b794b945fc678e7782fb9848518cd27
Rishika <rishikajoshi@yahoo.com>
2025-11-12 13:22:03 -0500

    modified calculator

--- test_calculator.py
+++ test_calculator.py
@@ -57,22 +57,24 @@ class TestCalculator(unittest.TestCase):
 
     
     ######## Partner 1
-    # def test_log_invalid_argument(self): # 1 assertion
+    def test_log_invalid_argument(self): # 1 assertion
     #     # call log function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     logarithm(0, 5)
-    #     fill in code
+        with self.assertRaises(ValueError):
+            logarithm(2, -5)
+        with self.assertRaises(ValueError):
+            logarithm(3, 0)
 
-    # def test_hypotenuse(self): # 3 assertions
-    #     fill in code
 
-    # def test_sqrt(self): # 3 assertions
-    #     # Test for invalid argument, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #    square_root(NUM)
-    #     # Test basic function
-    #     fill in code
-    ##########################
+    def test_hypotenuse(self): # 3 assertions
+        self.assertEqual(hypotenuse(3, 4), 5)
+        self.assertEqual(hypotenuse(6, 8), 10)
+        self.assertEqual(hypotenuse(9, 12), 15)
+
+    def test_sqrt(self): # 3 assertions
+        with self.assertRaises(ValueError):
+            square_root(-9)
+        self.assertEqual(square_root(9), 3)
+        self.assertEqual(square_root(4), 2)
 
 # Do not touch this
 if __name__ == "__main__":
